4b99fdedf327858a070e3068ca4a110dcb5bc853
Ngsitjhb025 <ngsitjhb025@student.wethinkcode.co.za>
2026-09-16 21:37:04 +0200

    Added score, grading logic and unit testing

--- header-scanner/src/main/java/com/ngqabutho/headerscanner/model/ScanReport.java
+++ header-scanner/src/main/java/com/ngqabutho/headerscanner/model/ScanReport.java
@@ -1,4 +1,55 @@
 package com.ngqabutho.headerscanner.model;
 
-public class ScanReport {
+import java.util.List;
+
+public record ScanReport(
+        String url,
+        String finalUrl,
+        int statusCode,
+        List<HeaderFinding> findings
+) {
+
+    public int score(){
+        int score = 0;
+
+        for (HeaderFinding finding: findings){
+            Status status = finding.status();
+            Severity severity = finding.rule().severity();
+
+            if (severity.equals(Severity.HIGH)){
+                if (status.equals(Status.OK)){
+                    score += 3;
+                } else if (status.equals(Status.WEAK)){
+                    score += 1;
+                }
+            } else if (severity.equals(Severity.MEDIUM)){
+                if (status.equals(Status.OK)){
+                    score += 2;
+                } else if (status.equals(Status.WEAK)){
+                    score += 1;
+                }
+            }else{
+                if (status.equals(Status.OK)){
+                    score += 1;
+                }
+            }
+        }
+        return score;
+    }
+    
+    public char grade() {
+        int score = score();
+
+        if (score >= 12){
+            return 'A';
+        } else if (score >= 9){
+            return 'B';
+        } else if (score >= 6){
+            return 'C';
+        } else if (score >= 3){
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
--- header-scanner/src/test/java/com/ngqabutho/headerscanner/model/ScanReportTest.java
+++ header-scanner/src/test/java/com/ngqabutho/headerscanner/model/ScanReportTest.java
@@ -1,4 +1,331 @@
 package com.ngqabutho.headerscanner.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScanReportTest {
+
+    @Test
+    void highSeverityOkGetsThreePoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-Header",
+                Severity.HIGH,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.OK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+        );
+
+        int result = report.score();
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    void highSeverityWeakGetsOnePoint() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.HIGH,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.WEAK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void highSeverityMissingGetsNoPoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.HIGH,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.MISSING,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void mediumSeverityOkGetsTwoPoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.MEDIUM,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.OK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    void mediumSeverityWeakGetsOnePoint() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.MEDIUM,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.WEAK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void mediumSeverityMissingGetsNoPoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.MEDIUM,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.MISSING,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void lowSeverityOkGetsOnePoint() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.LOW,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.OK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void lowSeverityWeakGetsNoPoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.LOW,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.WEAK,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void lowSeverityMissingGetsNoPoints() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.LOW,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        HeaderFinding finding = new HeaderFinding(
+                rule,
+                Status.MISSING,
+                "test-value",
+                "test"
+        );
+
+        List<HeaderFinding> findings = List.of(finding);
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+
+        );
+
+        int result = report.score();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void gradeWhenScoreGreaterThanEleven() {
+        HeaderRule rule = new HeaderRule(
+                "test-header",
+                Severity.HIGH,
+                "test description",
+                "test recommendation",
+                null
+        );
+
+        List<HeaderFinding> findings = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            HeaderFinding finding = new HeaderFinding(
+                    rule,
+                    Status.OK,
+                    "test-value",
+                    "test"
+            );
+            findings.add(finding);
+        }
+
+        ScanReport report = new ScanReport(
+                "https://example.com",
+                "https://example.com",
+                200,
+                findings
+        );
+
+        char grade = report.grade();
+
+        assertEquals('A', grade);
+    }
+
 }
